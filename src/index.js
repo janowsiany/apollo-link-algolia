ed19f45309aeff9e1f32b4afefc24e0567dd8bee
Andrei Bunulu <bunulu@gmail.com>
2020-08-31 17:27:40 +0200

    feat: :rocket: Added multiple queries handling

--- src/index.js
+++ src/index.js
@@ -4,96 +4,112 @@ import { addTypenameToDocument, getMainDefinition, hasDirectives } from 'apollo-
 import { graphql } from 'graphql-anywhere/lib/async'
 
 const parameters = [
-  'advancedSyntax',
-  'allowTyposOnNumericTokens',
-  'analytics',
-  'analyticsTags',
-  'aroundLatLng',
-  'aroundLatLngViaIP',
-  'aroundPrecision',
-  'aroundRadius',
-  'attributesToHighlight',
-  'attributesToRetrieve',
-  'attributesToSnippet',
-  'disjunctiveFacets',
-  'distinct',
-  'facets',
-  'filters',
-  'getRankingInfo',
-  'hitsPerPage',
-  'ignorePlurals',
-  'insideBoundingBox',
-  'insidePolygon',
-  'maxValuesPerFacet',
-  'minimumAroundRadius',
-  'minWordSizefor1Typo',
-  'minWordSizefor2Typos',
-  'optionalWords',
-  'page',
-  'query',
-  'queryType',
-  'removeWordsIfNoResults',
-  'replaceSynonymsInHighlight',
-  'restrictSearchableAttributes',
-  'synonyms',
-  'tagFilters',
-  'typoTolerance'
+    'advancedSyntax',
+    'allowTyposOnNumericTokens',
+    'analytics',
+    'analyticsTags',
+    'aroundLatLng',
+    'aroundLatLngViaIP',
+    'aroundPrecision',
+    'aroundRadius',
+    'attributesToHighlight',
+    'attributesToRetrieve',
+    'attributesToSnippet',
+    'disjunctiveFacets',
+    'distinct',
+    'facets',
+    'filters',
+    'getRankingInfo',
+    'hitsPerPage',
+    'ignorePlurals',
+    'insideBoundingBox',
+    'insidePolygon',
+    'maxValuesPerFacet',
+    'minimumAroundRadius',
+    'minWordSizefor1Typo',
+    'minWordSizefor2Typos',
+    'optionalWords',
+    'page',
+    'query',
+    'queryType',
+    'removeWordsIfNoResults',
+    'replaceSynonymsInHighlight',
+    'restrictSearchableAttributes',
+    'synonyms',
+    'tagFilters',
+    'typoTolerance'
 ]
 
-const addTypeNameToResult = (result, __typename = 'AlgoliaQuery') => Object.assign({}, result.content, { __typename })
+const prettifyResult = ({ result, type: __typename = 'AlgoliaQuery' }) => Object.assign({}, result.content ? result.content : result, { __typename })
+
+
+const hasIndex = directives => {
+    const { queries, index } = directives.algolia
+
+    return index || (Array.isArray(queries) && queries.every(q => q.indexName))
+}
 
 const resolver = (fieldName, root, args, context, info) => {
-  const { directives, isLeaf, resultKey } = info
+    const { directives, isLeaf, resultKey } = info
+    const isNotAlgoliaQuery = !directives || !directives.algolia
 
-  const isNotAlgoliaQuery = !directives || !directives.algolia
-  if (isLeaf || isNotAlgoliaQuery) {
-    const returnValue = (root || {})[resultKey] || (root || {})[fieldName]
-    return returnValue !== undefined ? returnValue : null
-  }
+    if (isLeaf || isNotAlgoliaQuery) {
+        const returnValue = (root || {})[resultKey] || (root || {})[fieldName]
 
-  if (!directives.algolia.index) {
-    throw new Error('Algolia index name is required')
-  }
-
-  const helper = algoliasearchHelper(context.client, directives.algolia.index)
-
-  parameters.forEach(parameter => {
-    if (parameter in directives.algolia) {
-      helper.setQueryParameter(parameter, directives.algolia[parameter])
+        return returnValue !== undefined ? returnValue : null
     }
-  })
 
-  return helper.searchOnce().then(result => addTypeNameToResult(result))
+    if (!hasIndex(directives)) {
+        throw new Error('Algolia index name is required')
+    }
+
+    const helper = algoliasearchHelper(context.client, directives.algolia.index)
+
+    // Multiple queries
+    if (directives.algolia.queries) {
+        return helper.client
+            .multipleQueries(directives.algolia.queries)
+            .then(result => prettifyResult({ result, type: directives.algolia.type }))
+    }
+
+    // Single query
+    parameters.forEach(parameter => {
+        if (parameter in directives.algolia) {
+            helper.setQueryParameter(parameter, directives.algolia[parameter])
+        }
+    })
+
+    return helper.searchOnce().then(result => prettifyResult({ result, type: directives.algolia.type }))
 }
 
 export default class AlgoliaLink extends ApolloLink {
-  constructor({ client }) {
-    super()
-    this.client = client
-  }
-
-  request(operation, forward) {
-    const isAlgoliaQuery = hasDirectives(['algolia'], operation.query)
-
-    if (!isAlgoliaQuery && forward) {
-      return forward(operation)
+    constructor({ client }) {
+        super()
+        this.client = client
     }
 
-    const context = {
-      client: this.client,
-      findType: directives => directives.algolia.type,
-      mainDefinition: getMainDefinition(operation.query) // note: not used at the moment
-    }
+    request(operation, forward) {
+        const isAlgoliaQuery = hasDirectives(['algolia'], operation.query)
 
-    return new Observable(observer => {
-      graphql(resolver, addTypenameToDocument(operation.query), {}, context, operation.variables)
-        .then(data => {
-          observer.next({ data })
-          observer.complete()
+        if (!isAlgoliaQuery && forward) {
+            return forward(operation)
+        }
+
+        const context = {
+            client: this.client,
+            findType: directives => directives.algolia.type,
+            mainDefinition: getMainDefinition(operation.query) // note: not used at the moment
+        }
+
+        return new Observable(observer => {
+            graphql(resolver, addTypenameToDocument(operation.query), {}, context, operation.variables)
+                .then(data => {
+                    observer.next({ data })
+                    observer.complete()
+                })
+                .catch(error => {
+                    observer.error(error)
+                })
         })
-        .catch(error => {
-          observer.error(error)
-        })
-    })
-  }
+    }
 }
